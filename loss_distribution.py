36385c0b5b9c018c19965d638dd8ef11d5c33fad
Landan Seguin <landan@mosaicml.com>
2024-02-01 21:10:05 -0800

    Loss distribution

---
+++ loss_distribution.py
@@ -0,0 +1,38 @@
+from diffusion.datasets.image_caption import build_streaming_image_caption_dataloader
+from diffusion.models.models import stable_diffusion_xl
+from tqdm import tqdm
+import torch
+
+remotes = 'oci://mosaicml-internal-dataset-laion2b-en/4.5v2/filter_v2/256-512/4.5-5.0/1'
+locals = '/tmp/4.5v2/filter_v2/256-512/4.5-5.0/1.3'
+
+print('Creating dataloader')
+dataloader = build_streaming_image_caption_dataloader(
+    remote=remotes,
+    local=locals,
+    batch_size=32,
+    image_key='jpg',
+    caption_key='caption',
+    tokenizer_name_or_path='stabilityai/stable-diffusion-xl-base-1.0',
+    streaming_kwargs={'shuffle': True},
+)
+print('Created Dataloader')
+
+print('Creating model')
+model = stable_diffusion_xl(fsdp=False, clip_qkv=None, loss_bins=[], val_guidance_scales=[], precomputed_latents=False, encode_latents_in_fp16=False)
+print('Created model')
+model = model.cuda()
+
+print('Loading batch')
+count = 0
+losses = {k: [] for k in range(1000)}
+with torch.no_grad():
+    for batch in tqdm(dataloader):
+        out = model(batch)
+        losses[out[-1].item()].append(model.loss(out, batch).item())
+        if count == 10000:
+            break
+        count += 1
+losses = {k: (torch.tensor(v).mean().item(), torch.tensor(v).std().item()) for k, v in losses.items()}
+print(losses)
+exit()
