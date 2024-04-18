af7da5bcb6338fb6bd9c3b2cc5bbec0e526af3ee
Landan Seguin <landan@mosaicml.com>
2024-04-17 17:13:16 -0700

    add noop model

---
+++ diffusion/models/noop.py
@@ -0,0 +1,39 @@
+# Copyright 2022 MosaicML Diffusion authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""NoOpModel algorithm and class."""
+
+from typing import Any, Dict, Optional
+
+import torch
+import torch.nn.functional as F
+from torchmetrics import Metric
+
+from composer.models.base import ComposerModel
+
+class NoOpModel(ComposerModel):
+    """No-op model used for performance measurements.
+    Args:
+        original_model (torch.nn.Module): Model to replace.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.weight = torch.nn.Linear(in_features=1, out_features=16)
+
+    def loss(self, outputs: torch.Tensor, batch):
+        y = torch.randn_like(self.weight.weight)
+        return F.mse_loss(outputs, y)
+
+    def forward(self, batch):
+        input = torch.randn_like(self.weight.weight).sum().unsqueeze(0)
+        return self.weight(input)
+
+    def get_metrics(self, is_train: bool) -> Dict[str, Metric]:
+        return {}
+
+    def eval_forward(self, batch, outputs: Optional[Any] = None):
+        return self.forward(batch)
+
+    def update_metric(self, batch: Any, outputs: Any, metric: Metric) -> None:
+        pass
